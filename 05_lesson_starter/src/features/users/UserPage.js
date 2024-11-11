57defb06002089222a97856a4dc841b9ae7c5b5d
Vamonosraza <martinezjessy393@gmail.com>
2024-11-11 13:58:42 -0800

    replaced useSelector with memoization

--- 05_lesson_starter/src/features/users/UserPage.js
+++ 05_lesson_starter/src/features/users/UserPage.js
@@ -7,10 +7,8 @@ const UserPage = () => {
     const { userId } = useParams(); // we are grabbing the userId from the url in the form of a string
     const user = useSelector(state => selectUserById(state, Number(userId))); // this is a selector that returns the user with the given id
 
-    const postsForUser = useSelector(state => { // we want to display the posts for this user
-        const allPosts = selectAllPosts(state);
-        return allPosts.filter(post => post.userId === Number(userId)) // find the result by the user id. useSelector will run everytime a an action is dispatched. Forces the component to re-render every time the state changes.
-    })
+    // using the memoized selectors to get the state
+    const postsForUser = useSelector(state => selectPostsByUser(state, Number(userId)));
 
     const postTitles = postsForUser.map(post => ( // link the posts to their own page
         <li key={post.id}>
